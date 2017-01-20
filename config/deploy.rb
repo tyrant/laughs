56e4697cfc691409323a81277464ace12bafcafe
Mikey Clarke <epicschnozz@gmail.com>
2017-01-20 23:04:07 +1300

    Added public/system to cap :linked_dirs

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,7 +24,7 @@ set :pty, true
 append :linked_files, "config/database.yml", "config/secrets.yml"
 
 # Default value for linked_dirs is []
-# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+append :linked_dirs, "public/system"#, "log", "tmp/pids", "tmp/cache", "tmp/sockets"
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
